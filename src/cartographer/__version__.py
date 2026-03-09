b21cceda3e5f2b0edd18c8b04d6254d2b18a9ee9
Jacob10383 <Jacob10383@users.noreply.github.com>
2026-03-08 22:45:53 -0700

    Set checked-in version for non-pip installs
    Set __version__.py in git because this install flow does not use pip / setuptools-scm to generate the runtime version.

--- src/cartographer/__version__.py
+++ src/cartographer/__version__.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "1.3.3"
-__version_tuple__ = version_tuple = (1, 3, 3)
+__version__ = version = "1.5.0-k2jacob10383-custom"
+__version_tuple__ = version_tuple = (1, 5, 0, "k2jacob10383", "custom")
 
 __commit_id__ = commit_id = None
